8fd170e611cdf835cc50870654c546f0fdbe0196
adkhiker <poirier.greg@gmail.com>
2019-06-18 18:04:29 -0400

    WIP: styling the page

--- js/index.js
+++ js/index.js
@@ -15,9 +15,6 @@ let p = document.querySelectorAll("p");
 let pArray = Array.from(p);
 let footer = document.getElementsByClassName("footer");
 
-
-
-
 imagesArray.forEach(function(image) {
   image.addEventListener("mouseenter", event => {
     image.style.border = "6px solid #a3f7b5";
@@ -30,16 +27,68 @@ imagesArray.forEach(function(image) {
 
 h1.addEventListener("mouseover", event => {
   h1.textContent = "RIDE THE FUN BUS!";
-  h1.style.color = "#a3f7b5";
+  h1.style.color = "#black";
   h1.style.fontWeight = "700";
-  h1.style.textShadow = "5px 10px yellow";
+  h1.style.textShadow = "5px 10px #FFE66D";
   event.stopPropagation();
 });
 h1.addEventListener("mouseout", event => {
   h1.textContent = "Choose an Adventure!";
-  h1.style.color = "black";
+  h1.style.color = "#1a535c";
   h1.style.fontWeight = "none";
-  h1.style.textShadow = "none";
+  //   h1.style.textShadow = "5px 10px #4ecdc4";
   event.stopPropagation();
 });
 
+navlinksArray.forEach(function(link) {
+  link.addEventListener("mouseover", event => {
+    link.style.color = "#a3f7b5";
+    link.style.fontWeight = "700";
+    link.style.textShadow = "5px 7px #FFE66D";
+    link.style.fontFamily = "'Indie Flower', cursive";
+  });
+  link.addEventListener("mouseout", event => {
+    link.style.color = "#FF6B6B";
+    link.style.fontWeight = "600";
+    link.style.textShadow = "none";
+    link.style.fontFamily = "none";
+  });
+  link.addEventListener("click", event => {
+    alert("That goes nowhere!!!");
+  });
+});
+
+pArray.forEach(P => {
+  P.addEventListener("dblclick", event => {
+    P.style.color = "#A30000";
+  });
+  
+ });
+
+textArea.forEach(ta => {
+  ta.addEventListener("mouseenter", event => {
+    ta.style.textAlign = "center";  
+    ta.style.backgroundColor = "#EFD28D";
+    ta.style.borderRadius = "5px";
+    ta.style.color = "#004777";
+  });
+  ta.addEventListener("mouseleave", event => {
+    ta.style.backgroundColor = "#a30000";
+    ta.style.textAlign = "left"; 
+    ta.style.color = "black";
+  });
+});
+
+destination.forEach(d => {
+  d.addEventListener("mouseenter", event => {
+    d.style.backgroundColor = "#00AFB5";
+    d.style.borderRadius = "15px";
+    d.style.color = "white";
+    d.style.textAlign = "center";
+  });
+  d.addEventListener("mouseleave", event => {
+    d.style.textAlign = "left";  
+    d.style.backgroundColor = "white";
+    d.style.color = "black";
+  });
+});
