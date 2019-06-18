0b13c35eaecf1932203621d1c584ac9ab55c823d
adkhiker <poirier.greg@gmail.com>
2019-06-18 17:08:22 -0400

    WIP: h1

--- js/index.js
+++ js/index.js
@@ -14,3 +14,32 @@ let destination = document.querySelectorAll(".destination");
 let p = document.querySelectorAll("p");
 let pArray = Array.from(p);
 let footer = document.getElementsByClassName("footer");
+
+
+
+
+imagesArray.forEach(function(image) {
+  image.addEventListener("mouseenter", event => {
+    image.style.border = "6px solid #a3f7b5";
+    event.stopPropagation();
+  });
+  image.addEventListener("mouseleave", event => {
+    image.style.border = "none";
+  });
+});
+
+h1.addEventListener("mouseover", event => {
+  h1.textContent = "RIDE THE FUN BUS!";
+  h1.style.color = "#a3f7b5";
+  h1.style.fontWeight = "700";
+  h1.style.textShadow = "5px 10px yellow";
+  event.stopPropagation();
+});
+h1.addEventListener("mouseout", event => {
+  h1.textContent = "Choose an Adventure!";
+  h1.style.color = "black";
+  h1.style.fontWeight = "none";
+  h1.style.textShadow = "none";
+  event.stopPropagation();
+});
+
