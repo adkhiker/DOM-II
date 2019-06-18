f1e9b6ce1ce7908fcc52f17bf0e462213d954fff
adkhiker <poirier.greg@gmail.com>
2019-06-18 16:01:34 -0400

    WIP: Selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+let h1 = document.querySelector(".logo-heading");
+let navblock = document.querySelector(".nav");
+let navlinks = document.querySelectorAll(".nav-link");
+let navlinksArray = Array.from(navlinks);
+let images = document.querySelectorAll("img");
+let imagesArray = Array.from(images);
+let intro = document.querySelector(".intro");
+let h2 = document.querySelectorAll("h2");
+let h4 = document.querySelectorAll("h4");
+let textArea = document.querySelectorAll(".text-content");
+let destination = document.querySelectorAll(".destination");
+let p = document.querySelectorAll("p");
+let pArray = Array.from(p);
+let footer = document.getElementsByClassName("footer");
